e087389883faa4f1825504da487fa75b2b85100f
Howard Ricketts <molorosh@gmail.com>
2019-04-19 20:55:22 +0100

    adding some db field notes

--- react-app/src/db.js
+++ react-app/src/db.js
@@ -7,3 +7,22 @@ const db = new Dexie('MoloroshPharmaDB');
 db.version(1).stores({ persons: '++id' });
 
 export default db;
+
+// persons:
+//   id
+//   icon
+//   color
+//   
+
+// meds
+//   id (1)
+//   personid (2)
+//   name ("aspirin")
+//   strength ("500mg")
+//   units ("tablets" of "tablets"/"ml")
+//   stockDate ("2018-01-23")
+//   stockAmount (23) 
+//   scheduleAmount (2)
+//   everyNDays (1)
+//   predictedLastFullDoseDate ("2018-05-01")
+
