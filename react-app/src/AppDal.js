9efac43b6e4d65943bac94dbbf9630f636f6f080
Howard Ricketts <molorosh@gmail.com>
2019-04-27 11:58:26 +0100

    started to create shared validation functions

--- react-app/src/AppDal.js
+++ react-app/src/AppDal.js
@@ -1,5 +1,6 @@
 import db from './db';
-
+// TODO - at some point should split this into a Business Logic Layer
+// and a (hidden) Data Access Layer
 class AppDal {
     
     isEmptyOrSpaces(str){
@@ -60,20 +61,37 @@ class AppDal {
         return errors;
     }
 
-    addMedication = (medicineId, txtRestock) => {
-        let errors = [];
+    mustBePositiveInteger = (intText, name) => {
+        let ret = { 
+            isValid: false,
+            value: undefined,
+            msg: undefined
+        }
         let lvl = undefined;
-        if(this.isEmptyOrSpaces(txtRestock)){
-            errors.push({name: "errRestock", msg: "You must supply a restock level" });
+        if(this.isEmptyOrSpaces(intText)){
+            ret.msg = "You must supply a " + name;
         }else{
-            lvl = parseInt(txtRestock, 10);
-            if(lvl && lvl.toString && lvl.toString() === txtRestock){
+            lvl = parseInt(intText, 10);
+            if(lvl && lvl.toString && lvl.toString() === intText){
                 if(lvl < 1){
-                    errors.push({name: "errRestock", msg: "Restock level must be more than zero" });
+                    ret.msg = name + " must be more than zero";
+                }
+                else{
+                    ret.isValid = true;
+                    ret.value = lvl;
                 }
             }else{
-                errors.push({name: "errRestock", msg: "You must supply a valid restock level" });
+                ret.msg = "You must supply a valid " + name;
             }
+        }
+        return ret;
+    }
+
+    addMedication = (medicineId, txtRestock) => {
+        let errors = [];
+        let restockValidation = this.mustBePositiveInteger(txtRestock, "Restock Level");
+        if(!restockValidation.isValid){
+            errors.push({name: "err_Restock", msg: restockValidation.msg });
         }
         if(errors.length === 0){
             // do the update
