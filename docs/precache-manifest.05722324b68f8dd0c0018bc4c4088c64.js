5783e09c8f6ea649de4d2320e6853efe3e50fc31
Howard Ricketts <molorosh@gmail.com>
2019-04-27 20:35:41 +0100

    published v 0.6.1.0 to github pages

--- docs/precache-manifest.05722324b68f8dd0c0018bc4c4088c64.js
+++ docs/precache-manifest.05722324b68f8dd0c0018bc4c4088c64.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/pharma/static/js/runtime~main.ace989df.js"
   },
   {
-    "revision": "ac9486b7ab8afe464820",
-    "url": "/pharma/static/js/main.4b563095.chunk.js"
+    "revision": "3e0b6ae74bb2e7bf16f4",
+    "url": "/pharma/static/js/main.fb3ae273.chunk.js"
   },
   {
     "revision": "e86b20240886639f4321",
     "url": "/pharma/static/js/2.2cd8b3ed.chunk.js"
   },
   {
-    "revision": "ac9486b7ab8afe464820",
+    "revision": "3e0b6ae74bb2e7bf16f4",
     "url": "/pharma/static/css/main.4853683d.chunk.css"
   },
   {
-    "revision": "4c96be45bc943e658237ced011873c8c",
+    "revision": "a61956c6e0b9a9b7475e4b2aa533aee1",
     "url": "/pharma/index.html"
   }
 ];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pharma/precache-manifest.a39bde5bf6a3827afdc680b5eda0fbd7.js"
+  "/pharma/precache-manifest.05722324b68f8dd0c0018bc4c4088c64.js"
 );
 
 workbox.clientsClaim();
