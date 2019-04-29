708ab758ff4a6a30ab43dd34f7928ef32160ada5
Howard Ricketts <molorosh@gmail.com>
2019-04-30 00:23:00 +0100

    added documentation; v 0.9.0.0

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -5,8 +5,11 @@ class About extends Component {
     
     render(){
         return (
-            <>
+            <div className="theAboutDiv">
                 <h3><span role="img" aria-label="About">📜</span> About</h3>
+                <p>This web app was written by <a href="http://www.molorosh.com">molorosh</a>.</p>
+                <p>If you're a geeky kinda person the source code for the web app is viewable on <a href="https://github.com/molorosh/pharma">github</a></p>
+                <p>&copy; 2019 molorosh</p>
                 <p>
                     <button 
                         onClick={this.props.doClose} 
@@ -15,7 +18,7 @@ class About extends Component {
                         Close
                     </button>
                 </p>
-            </>
+            </div>
         );
     }
 
--- react-app/src/Documentation.js
+++ react-app/src/Documentation.js
@@ -5,10 +5,66 @@ class Documentation extends Component {
 
     render(){
         return (
-            <>
-                <h3><span role="img" aria-label="Help">📖</span> Help</h3>
-                <p></p>
-
+            <div className="theHelpDiv">
+                <h3><span role="img" aria-label="Help">❓</span> Help</h3>
+                <h4><span role="img" aria-label="Purpose">🐬</span>Purpose</h4>
+                <p>
+                    This is a small web app to track multiple medication prescriptions for one or more people, 
+                    and to show when medicines are predicted to run out.
+                </p>
+                <h4><span role="img" aria-label="Instructions">📖</span>Instructions</h4>
+                <p>
+                    The <b>New Person</b> button lets you create a new person.  No need to give the full name, 
+                    just initials or a nickname will do.</p>
+                    <p>
+                    The <b>Add Medicine</b> button lets you add a medication for a person.  Just fill out the 
+                    details and it will work how many full days of days are left before you must get another 
+                    prescription.  If the person has several pills throughout the day, just give the daily total.
+                </p>
+                <p>
+                    The <b>Restock Medicine</b> button lets you correct the amount of a medicine.  Please enter the "new" 
+                    stock leval at the start of the day (i.e. include, as stock, any meds you have already given <u>today</u>).
+                </p>
+                <p>
+                    The <b>Delete Medicine</b> button will allow you to remove a medication from a user.
+                </p>
+                <p>
+                    The <b>Delete Person</b> button will allow you to remove a person and all their medication
+                     from the web app.
+                </p>
+                <h4><span role="img" aria-label="Warning">⚠️</span>Oops!</h4>
+                <p>
+                    Deletions (and Additions and Restocks) always go to a confirmation screen, so don't worry 
+                    about accididentally hitting a button - just click the <b>Cancel</b> button.
+                </p>
+                <h4><span role="img" aria-label="Data">💾</span>Data</h4>
+                <p>
+                    Because medication details are highly personal medical data, 
+                    this app does not store any data remotely.  
+                    All prescription and stock level data is stored locally 
+                    on the phone or tablet or computer you are currently looking at.
+                </p>
+                <p>
+                    This does however mean that if you set up all the information on your phone, 
+                    it will not appear in your tablet, so please take a moment to choose if it's 
+                    best for you to keep the information on your personal smartphone,
+                     or on a shared tablet or computer.
+                </p>
+                <h4><span role="img" aria-label="Email">📧</span>Feedback</h4>
+                <p>
+                    The app email address is: <a href="mailto:pharma@molorosh.com">pharma@molorosh.com</a>.
+                    Please feel free to send an email with any feedback, comments, problems, 
+                    questions or new feature suggestions.
+                </p>
+                <h4><span role="img" aria-label="Crystal Ball">🔮</span>Future Features</h4>
+                <p>
+                    The web app does not currently support PRN (<i>Pro Re Nata</i> - Take As Required) medications.
+                    That capability is being worked on at the moment, so it will appear sometime in <b>May 2019</b>.  Promise.
+                </p>
+                <h4><span role="img" aria-label="Free of Charge">🈚</span>Free of Charge</h4>
+                <p>
+                    This web app is free to use and always will be :-)
+                </p>
                 <p>
                     <button 
                         onClick={this.props.doClose} 
@@ -17,7 +73,7 @@ class Documentation extends Component {
                         Close
                     </button>
                 </p>
-            </>
+            </div>
         );
     }
 
